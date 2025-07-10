94a0ef8b237aeefc8310acc8db1d2e2ee73e599b
Un Kyu Lee <unkyulee@gmail.com>
2025-07-10 14:31:44 +0200

    some progress on the initial setup phase

--- platformio/src/display/WiFiScreen/WiFiScreen.cpp
+++ platformio/src/display/WiFiScreen/WiFiScreen.cpp
@@ -17,9 +17,20 @@ void WiFiScreen_render(TFT_eSPI *ptft)
     ///
     ptft->setCursor(0, 0, 2);
     ptft->setTextSize(1); // Adjust text size as needed
-    ptft->setTextColor(TFT_WHITE, TFT_RED);
+    ptft->setTextColor(TFT_WHITE, TFT_BLACK);
 
     // Draw text in the middle of the screen
-    ptft->setCursor(0, 0, 2);
-    ptft->print("WiFi Setup");
+    ptft->setCursor(2, 0, 2);
+
+    if (app["wifi"])
+    {
+        ptft->print("WiFi connected");
+        // wifi is connected
+        // go ahead and perform the first downloads and display the clock screen
+    }
+    else
+    {
+        ptft->print("WiFi not connected");
+        // make wifi access point and display the IP information
+    }
 }
